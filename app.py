094df0be7c9003a25590dde0cf8501d39cb18bd9
Vojtech <vojtech.deml@gmail.com>
2026-03-09 22:38:48 +0100

    Added register.html, iported werkzeug.security

--- app.py
+++ app.py
@@ -1,8 +1,13 @@
 import click
-from flask import Flask, g, render_template, request, redirect, session, current_app
-from helpers import apology, login_required, usd
 import os
 import sqlite3
+
+from flask import Flask, g, render_template, request, redirect, session, current_app
+from helpers import apology, login_required, usd
+from werkzeug.security import check_password_hash, generate_password_hash
+
+
+
 
 app = Flask(__name__, instance_relative_config=True)
 
@@ -19,6 +24,7 @@ def init_db():
         with open("schema.sql") as f:
             db.executescript(f.read())
 
+# Initialize database with command-line interface
 @app.cli.command("init-db")
 def init_db_command():
     """ Create database tables """
@@ -29,6 +35,31 @@ def init_db_command():
 @app.route("/")
 def index():
     return render_template("index.html")
+
+
+@app.route("/register", methods=["GET", "POST"])
+def register():
+    """Register user"""
+    if request.method == "POST":
+        if not request.form.get("username"):
+            return apology("must provide a username")
+        
+        if not request.form.get("password"):
+            return apology("must provide a password")
+        
+        if not request.form.get("confirmation"):
+            return apology("must provide password connfirmation")
+        
+        if not request.form.get("password") == request.form.get("confirmation"):
+            return apology("Password and passwrod confirmation must match")
+
+
+
+    else:
+        return render_template("register.html")
+
+
+
 
 if __name__ == "__main__":
     app.run(debug=True)
