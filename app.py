0425e903c70d2f5e184248a1912795d3af88d753
Vojtech <vojtech.deml@gmail.com>
2026-03-13 23:29:34 +0100

    added more static folders, made improvements to registration

--- app.py
+++ app.py
@@ -18,7 +18,7 @@ app.config["DATABASE"] = os.path.join(app.instance_path, "database.db")
 # Ensure instance folder exists
 os.makedirs(app.instance_path, exist_ok=True)
 
-""" Adding a function to run Schema.sql"""
+"""Adding a function to run Schema.sql"""
 def init_db():
     with sqlite3.connect(app.config["DATABASE"]) as db:
         with open("schema.sql") as f:
@@ -38,13 +38,15 @@ def index():
         cursor = conn.cursor()
 
         cursor.execute("SELECT * FROM users")
-        row = cursor.fetchall()
-
+        row = cursor.fetchone()
+        cursor.execute("SELECT username, email FROM users")
+        rows = cursor.fetchall()
+        
 
         conn.commit()
         conn.close()
 
-        return render_template("index.html", row = row)
+        return render_template("index.html", rows = rows, row = row)
 
 
 @app.route("/register", methods=["GET", "POST"])
@@ -62,15 +64,22 @@ def register():
         
         if not request.form.get("password") == request.form.get("confirmation"):
             return apology("Password and passwrod confirmation must match")
-
+        
+        # ensure username and email are unique
         username = request.form.get("username")
         email = request.form.get("email")
-        password_hash = generate_password_hash(request.form.get("password"), method='scrypt', salt_length=16)
-
-        # sets up a connection to the database
         conn = sqlite3.connect(app.config["DATABASE"])
         cursor = conn.cursor()
-        
+        cursor.execute("SELECT username FROM users WHERE username = ?", (username, ))
+        username_check = cursor.fetchall()
+        if len(username_check) != 0:
+            return apology("Username taken", 400)
+        cursor.execute("SELECT email FROM users WHERE email = ?", (email, ))
+        email_check = cursor.fetchall()
+        if len(email_check) != 0:
+            return apology("Email taken", 400)
+        password_hash = generate_password_hash(request.form.get("password"), method='scrypt', salt_length=16)
+       
         # inserts a new row into the users table with newly registered users credentials
         cursor.execute("INSERT INTO users (username, email, password_hash) VALUES (?, ?, ?)", (username, email, password_hash))
 
